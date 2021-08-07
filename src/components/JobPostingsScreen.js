e80e857a6d71c5a7e4f5cf031d27c1ed886caca1
Alex <lialex0218@gmail.com>
2021-08-07 13:09:43 -0700

    still working on combining erros

--- src/components/JobPostingsScreen.js
+++ src/components/JobPostingsScreen.js
@@ -1,88 +1,114 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, ScrollView, SafeAreaView } from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack'
 
 const newPosting = createStackNavigator();
 
 
-function JobPostingsScreen() {
+function JobPostingsScreen({ navigation, route }) {
+
+  const [JobPost, setJobPost] = useState({
+    title: '',
+    industry: '',
+    description: '',
+  })
+  const { title, industry, description } = route.params
+
+  function addJobs() {
+    console.log('add jobs title', title)
+    const newPost = {
+      title: title,
+      industry: industry,
+      description: description
+    }
+    setJobPost((prev) => [...prev, newPost])
+
+
+
+    return addJobs;
+  }
+
+
+
+
   return (
 
-    
-    <View style={ {flex:1 }}> 
-    
+
+    <View style={{ flex: 1 }}>
+
       <View >
         <Text style={styles.header}>
           Your Job Postings!
+          {console.log('in the job positing', route.params.description)}
         </Text>
       </View>
-{/* --------------------------------Posting 1-------------------------------------- */}
+      {/* --------------------------------Posting 1-------------------------------------- */}
       <ScrollView>
         <View style={styles.jobpost}>
-          <Text style={{...styles.title, color: 'black'}}>
+          <Text style={{ ...styles.title, color: 'black' }}>
             Full Stack Web Developer
           </Text>
 
-          <Text style={{...styles.description, color: 'black'}}>
+          <Text style={{ ...styles.description, color: 'black' }}>
             Industry:
           </Text>
 
-          <Text style={{...styles.description, color: 'black'}}>
+          <Text style={{ ...styles.description, color: 'black' }}>
             Job Description:
           </Text>
 
-            <Text style={{...styles.text, color: 'black'}}>
-              Developer with several years developing for the web. Must have a passion for creating responsive, user friendly web interfaces.
-              Strong understanding of javascript (outside of a framework)-Good understanding of web application lifecycles-Experience in, React, Typescript, Node.js, Bootstrap, CSS, LESS, HTML-Development experience in C#, ASP.net core-Ability to debug and understand existing code bases.
-            </Text>
-          
+          <Text style={{ ...styles.text, color: 'black' }}>
+            Developer with several years developing for the web. Must have a passion for creating responsive, user friendly web interfaces.
+            Strong understanding of javascript (outside of a framework)-Good understanding of web application lifecycles-Experience in, React, Typescript, Node.js, Bootstrap, CSS, LESS, HTML-Development experience in C#, ASP.net core-Ability to debug and understand existing code bases.
+          </Text>
+
         </View>
 
-{/* --------------------------------Posting 2-------------------------------------- */}
-  
-        <View style={styles.jobpost}>
-          <Text style={{...styles.title, color: 'black'}}>
-            Full Stack Web Developer
-          </Text>
-
-          <Text style={{...styles.description, color: 'black'}}>
-            Industry:
-          </Text>
-
-          <Text style={{...styles.description, color: 'black'}}>
-            Job Description:
-          </Text>
-
-            <Text style={{...styles.text, color: 'black'}}>
-              Developer with several years developing for the web. Must have a passion for creating responsive, user friendly web interfaces.
-              Strong understanding of javascript (outside of a framework)-Good understanding of web application lifecycles-Experience in, React, Typescript, Node.js, Bootstrap, CSS, LESS, HTML-Development experience in C#, ASP.net core-Ability to debug and understand existing code bases.
-              Developer with several years developing for the web. Must have a passion for creating responsive, user friendly web interfaces.
-              Strong understanding of javascript (outside of a framework)-Good understanding of web application lifecycles-Experience in, React, Typescript, Node.js, Bootstrap, CSS, LESS, HTML-Development experience in C#, ASP.net core-Ability to debug and understand existing code bases.
-            </Text>
-        </View>
-
-{/* --------------------------------Posting 3-------------------------------------- */}
+        {/* --------------------------------Posting 2-------------------------------------- */}
 
         <View style={styles.jobpost}>
-          <Text style={{...styles.title, color: 'black'}}>
+          <Text style={{ ...styles.title, color: 'black' }}>
             Full Stack Web Developer
           </Text>
 
-          <Text style={{...styles.description, color: 'black'}}>
+          <Text style={{ ...styles.description, color: 'black' }}>
             Industry:
           </Text>
 
-          <Text style={{...styles.description, color: 'black'}}>
+          <Text style={{ ...styles.description, color: 'black' }}>
             Job Description:
           </Text>
 
-            <Text style={{...styles.text, color: 'black'}}>
-              Developer with several years developing for the web. Must have a passion for creating responsive, user friendly web interfaces.
-              Strong understanding of javascript (outside of a framework)-Good understanding of web application lifecycles-Experience in, React, Typescript, Node.js, Bootstrap, CSS, LESS, HTML-Development experience in C#, ASP.net core-Ability to debug and understand existing code bases.
-            </Text>
+          <Text style={{ ...styles.text, color: 'black' }}>
+            Developer with several years developing for the web. Must have a passion for creating responsive, user friendly web interfaces.
+            Strong understanding of javascript (outside of a framework)-Good understanding of web application lifecycles-Experience in, React, Typescript, Node.js, Bootstrap, CSS, LESS, HTML-Development experience in C#, ASP.net core-Ability to debug and understand existing code bases.
+            Developer with several years developing for the web. Must have a passion for creating responsive, user friendly web interfaces.
+            Strong understanding of javascript (outside of a framework)-Good understanding of web application lifecycles-Experience in, React, Typescript, Node.js, Bootstrap, CSS, LESS, HTML-Development experience in C#, ASP.net core-Ability to debug and understand existing code bases.
+          </Text>
         </View>
 
+        {/* --------------------------------Posting 3-------------------------------------- */}
 
+        <View style={styles.jobpost}>
+          <Text style={{ ...styles.title, color: 'black' }}>
+            Full Stack Web Developer
+          </Text>
+
+          <Text style={{ ...styles.description, color: 'black' }}>
+            Industry:
+          </Text>
+
+          <Text style={{ ...styles.description, color: 'black' }}>
+            Job Description:
+          </Text>
+
+          <Text style={{ ...styles.text, color: 'black' }}>
+            Developer with several years developing for the web. Must have a passion for creating responsive, user friendly web interfaces.
+            Strong understanding of javascript (outside of a framework)-Good understanding of web application lifecycles-Experience in, React, Typescript, Node.js, Bootstrap, CSS, LESS, HTML-Development experience in C#, ASP.net core-Ability to debug and understand existing code bases.
+          </Text>
+        </View>
+        {/* --------------------------------New Job Postin -------------------------------------- */}
+        {addJobs()}
       </ScrollView>
     </View>
   );
